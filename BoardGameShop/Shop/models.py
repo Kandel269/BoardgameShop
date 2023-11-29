07c827ebab2269c47152329e9892fb6b962452c3
Dominik <24534yrut@gmail.com>
2023-11-29 17:42:49 +0100

    models

--- BoardGameShop/Shop/models.py
+++ BoardGameShop/Shop/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='game_images/')
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    games = models.ManyToManyField(Game, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    games = models.ManyToManyField(Game, through='OrderItem')
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order #{self.pk}"
+
+class OrderItem(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.game.title}'
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    games = models.ManyToManyField(Game, through='CartItem')
+
+    def __str__(self):
+        return f'Cart for {self.user.username}'
+
+class CartItem(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.game.title} in cart'
