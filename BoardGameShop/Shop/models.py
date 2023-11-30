7c5cfc746cee7d6ce4260b6f230db41caa0abc4e
Dominik <24534yrut@gmail.com>
2023-11-30 06:40:11 +0100

    -

--- BoardGameShop/Shop/models.py
+++ BoardGameShop/Shop/models.py
@@ -9,6 +9,7 @@ class Game(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     image = models.ImageField(upload_to='game_images/')
+    published_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.title
--- BoardGameShop/Shop/views.py
+++ BoardGameShop/Shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from .models import *
 
-# Create your views here.
+class HomeView(View):
+    def get(self, request):
+        games = Game.objects.all().order_by('-published_date')
+        context = {'games': games}
+        return render(request,'home.html',context)
