e9bb3558981af0c44b7ee0300bda57f4f24cec8e
Dominik <24534yrut@gmail.com>
2023-12-28 15:19:32 +0100

    button logout

--- BoardGameShop/members/views.py
+++ BoardGameShop/members/views.py
@@ -45,10 +45,11 @@ class RegisterPage(View):
 
 class LogoutUser(View):
     def get(self, request):
-        logout(request,)
+        logout(request)
         return redirect('home')
 
 class AccountPage(View):
     template_name = 'account.html'
     def get(self, request):
         return render(request,'account.html')
+
